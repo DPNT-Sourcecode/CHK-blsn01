e8a49ee0c8fe5775db0d19a98d5595ed6c29a106
sbx_user1051 <sbx_user1051@169.254.9.201>
2022-03-13 18:17:59 +0000

    Sun Mar 13 18:17:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,28 +3,24 @@ from typing import List, Callable, Set
 
 
 class Basket:
-    def __init__(self, valid_skus: Set[str], offers: List[Callable]):
+    def __init__(self, skus: Dict[str, int], offers: List[Callable]):
         self.counter = Counter()
-        self.valid_skus = valid_skus
+        self.skus = skus
         self.offers = offers
 
     def put(sku: str) -> bool:
-        if sku not in self.valid_skus:
+        if sku not in self.skus:
             return False
         self.counter += sku
         
         return True
     
-    def 
+    def value() -> int:
+        pass
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-
-    final_price = 0
-    valid_skus = { 'A', 'B', 'C', 'D '}
-
-    counter = Counter()
 
     for sku in skus:
 
@@ -38,10 +34,3 @@ def checkout(skus):
         if counter['B'] == 2:
             counter['B'] -= 2
             counter['2B'] += 1
-
-
-
-
-
-
-

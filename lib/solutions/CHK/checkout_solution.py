96a8525154d2ccfebc5d1562ac2bd490c773f18e
sbx_user1051 <sbx_user1051@169.254.9.201>
2022-03-13 18:08:59 +0000

    Sun Mar 13 18:08:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,10 @@
 from collections import Counter
 
 
+class Basket:
+    def __init__(self):
+        self.counter = Counter()
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -16,6 +20,16 @@ def checkout(skus):
             return -1
 
         counter[sku] += 1
+
+        # check for 
+        if counter['A'] == 3:
+            counter['A'] -= 3
+            counter['3A'] += 1
+        
+        if counter['B'] == 2:
+            counter['B'] -= 2
+            counter['2B'] += 1
+
 
 
 

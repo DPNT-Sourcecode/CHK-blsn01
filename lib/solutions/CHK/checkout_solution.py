f95d5782170a7079f451ddb931b00e1587402857
sbx_user1051 <sbx_user1051@169.254.9.201>
2022-03-13 18:05:59 +0000

    Sun Mar 13 18:05:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,10 +6,17 @@ from collections import Counter
 def checkout(skus):
 
     final_price = 0
+    valid_skus = { 'A', 'B', 'C', 'D '}
 
     counter = Counter()
 
     for sku in skus:
+        # check validity
+        if sku not in valid_skus:
+            return -1
+
+        counter[sku] += 1
+
 
 
 
